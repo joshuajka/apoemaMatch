c6c746036766b51d788d4d34d895a7a2db0aed12
Joshua Abbate <55669412+joshuajka@users.noreply.github.com>
2022-08-15 21:14:17 -0300

    Ajuste css

---
+++ apoemaMatch/Migrations/20220815234548_mudancaBanco.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apoemaMatch.Migrations
+{
+    public partial class mudancaBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
