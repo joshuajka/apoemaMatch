41d27778534039566ae6c747ced0bad27e0096ad
Joshua Abbate <55669412+joshuajka@users.noreply.github.com>
2022-08-15 23:02:59 -0300

    Account Controller

---
+++ apoemaMatch/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using apoemaMatch.Data;
+using apoemaMatch.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apoemaMatch.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager; 
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
