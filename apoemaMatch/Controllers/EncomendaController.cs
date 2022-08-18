1be04edc62a97c78858967b5caed58a2556da484
Jonathan Miguel Ribeiro <jonathangoias01@hotmail.com>
2022-08-18 20:06:40 -0300

    Criar Controller e View da Encomenda

---
+++ apoemaMatch/Controllers/EncomendaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apoemaMatch.Controllers
+{
+    public class EncomendaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
