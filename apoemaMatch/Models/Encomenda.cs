657eb7f77b534fa1a531101be94a05a4dbb43013
Pedro Henrique Fernandes dos Reis <pedro0798henrique@gmail.com>
2022-08-18 21:50:46 -0300

    Cria model Encomenda

---
+++ apoemaMatch/Models/Encomenda.cs
@@ -0,0 +1,20 @@
+﻿using apoemaMatch.Data.Base;
+using apoemaMatch.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace apoemaMatch.Models
+{
+    public class Encomenda : IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public EnumSegmentoDeMercado AreaServico { get; set; }
+
+        public EnumAreaSolucaoBuscada ServicoBuscado { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
