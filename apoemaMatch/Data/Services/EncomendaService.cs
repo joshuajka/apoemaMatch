bdcfc81fc37f07a6afb8c852f8ba0d724588ac03
Pedro Henrique Fernandes dos Reis <pedro0798henrique@gmail.com>
2022-08-18 21:51:01 -0300

    Cria serviço Encomenda

---
+++ apoemaMatch/Data/Services/EncomendaService.cs
@@ -0,0 +1,10 @@
+﻿using apoemaMatch.Data.Base;
+using apoemaMatch.Models;
+
+namespace apoemaMatch.Data.Services
+{
+    public class EncomendaService : EntityBaseRepository<Encomenda>, IEncomendaService
+    {
+        public EncomendaService(AppDbContext context) : base(context) { }
+    }
+}
