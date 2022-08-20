fbb015edeb6f9cbf8a14a32d49203c2d31b4add6
Alan Viana <alanviana.nascimento@gmail.com>
2022-08-19 22:09:41 -0300

    Fazer os enuns de status

--- apoemaMatch/Models/Encomenda.cs
+++ apoemaMatch/Models/Encomenda.cs
@@ -17,6 +17,12 @@ namespace apoemaMatch.Models
         public EnumAreaSolucaoBuscada AreaSolucaoBuscada { get; set; }
 
         public string Descricao { get; set; }
+        
+       //TODO(Fazer os enuns de status)
+        //Iniciado -> Demandante criou a encomenda,
+       //Recusado -> Agenciador recusou a encomenda,
+       //Publico -> Encomenda disponível para os solucionaores,
+       //Finalizado -> encomenda deixou de ser disponível
 
         public bool RealizaProcessoSeletivo { get; set; }
 
