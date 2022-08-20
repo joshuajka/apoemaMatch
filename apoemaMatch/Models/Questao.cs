05d288e78b1700fe875055657243ba4fc3adae77
Alan Viana <alanviana.nascimento@gmail.com>
2022-08-19 21:35:00 -0300

    update questoes

--- apoemaMatch/Models/Questao.cs
+++ apoemaMatch/Models/Questao.cs
@@ -1,17 +1,18 @@
 ﻿using apoemaMatch.Data.Enums;
 using apoemaMatch.Data.Base;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace apoemaMatch.Models
 {
-    public class QuestionarioSelecao:IEntityBase
+    public class Questao:IEntityBase
     {
         [Key]
         public int Id { get; set; }
         
         public string Pergunta { get; set; }
         
-        public EnumTipoRespostaQuestionario tipoResposta { get; set; } 
+        public EnumTipoResposta tipoResposta { get; set; }
         
     }
 }
