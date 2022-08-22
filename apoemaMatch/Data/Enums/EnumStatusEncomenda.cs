16d9997732cd1d1ccd590114ab5c02ac6907d5d9
Alan Viana <alanviana.nascimento@gmail.com>
2022-08-22 20:27:02 -0300

    Incluir o status da encomenda

---
+++ apoemaMatch/Data/Enums/EnumStatusEncomenda.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apoemaMatch.Data.Enums
+{
+    public enum EnumStatusEncomenda
+    {
+        Iniciado,  // Demandante criou a encomenda,
+        Recusado, // Agenciador recusou a encomenda,
+        Publico, // Encomenda disponível para os solucionaores,
+        Finalizado // encomenda deixou de ser disponível
+    }
+}
--- apoemaMatch/Data/MetodosExtensao/ConversaoViewModelModel.cs
+++ apoemaMatch/Data/MetodosExtensao/ConversaoViewModelModel.cs
@@ -1,11 +1,12 @@
-﻿using apoemaMatch.Data.ViewModels;
+﻿using apoemaMatch.Data.Enums;
+using apoemaMatch.Data.ViewModels;
 using apoemaMatch.Models;
 
 namespace apoemaMatch.Data.MetodosExtensao
 {
     public static class ConversaoViewModelModel
     {
-        public static Encomenda Converta(this EncomendaViewModel encomendaViewModel)
+        public static Encomenda Converta(this EncomendaViewModel encomendaViewModel, EnumStatusEncomenda statusEncomenda = EnumStatusEncomenda.Iniciado)
         {
             return new()
             {
@@ -14,6 +15,7 @@ namespace apoemaMatch.Data.MetodosExtensao
                 SegmentoDeMercado = encomendaViewModel.SegmentoDeMercado,
                 AreaSolucaoBuscada = encomendaViewModel.AreaSolucaoBuscada,
                 Descricao = encomendaViewModel.Descricao,
+                StatusEncomenda = statusEncomenda,
                 RealizaProcessoSeletivo = encomendaViewModel.RealizaProcessoSeletivo,
                 Questoes = encomendaViewModel.Questoes?.ConvertAll(q => q.Converta())
             };
