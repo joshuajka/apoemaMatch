80f173bcd1042dd3b2b1459fcc029df801d6953f
Pedro Henrique Fernandes dos Reis <pedro0798henrique@gmail.com>
2022-08-29 03:16:38 -0300

    squad2 - adiciona migration para incluir questoes e opcoes de resposta

--- apoemaMatch/Migrations/20220829054208_QuestionarioSelecao.Designer.cs
+++ apoemaMatch/Migrations/20220829054208_QuestionarioSelecao.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using apoemaMatch.Data;
@@ -9,9 +10,10 @@ using apoemaMatch.Data;
 namespace apoemaMatch.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220829054208_QuestionarioSelecao")]
+    partial class QuestionarioSelecao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ apoemaMatch/Migrations/20220829054208_QuestionarioSelecao.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace apoemaMatch.Migrations
+{
+    public partial class QuestionarioSelecao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OpcaoResposta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Texto = table.Column<string>(type: "text", nullable: true),
+                    EhRespostaEsperada = table.Column<bool>(type: "boolean", nullable: false),
+                    QuestaoId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OpcaoResposta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OpcaoResposta_Questao_QuestaoId",
+                        column: x => x.QuestaoId,
+                        principalTable: "Questao",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpcaoResposta_QuestaoId",
+                table: "OpcaoResposta",
+                column: "QuestaoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OpcaoResposta");
+        }
+    }
+}
