8de968bcb1b4bf20ffb2bb8d8c19223bd2d75df8
lucianoSF <lukfraga@hotmail.com>
2022-08-29 17:54:29 -0300

    Corrigir botão de adicionar opções em cada questão

--- apoemaMatch/wwwroot/js/FormularioAvaliacao/formularioAvaliacao.js
+++ apoemaMatch/wwwroot/js/FormularioAvaliacao/formularioAvaliacao.js
@@ -23,10 +23,45 @@ function AddInput() {
     inputList.appendChild(element);
 }
 
+function AddOption() {
+    $(document).ready(function () {
+        $('.input-element button').one('click', function () {
+            //var txt= $(this).closest('.input-element').get();
+            $(this).closest('.input-element').find('.check-options').append(`<div class='form-check'> \
+                                                                                               <input class='form-check-input' type='checkbox' value='' name='flexCheckDefault' id='flexCheckDefault1'> \
+                                                                                                <div class='input-group mb-3'>
+                                                                                                    <input type='text' class='form-control form-control-sm'>
+                                                                                                    <div class="input-group-append">
+                                                                                                        <button class='btn btn-sm rmv_option' onclick='RemoveOption()' type='button' style='color: #f30e3a'>X</i></button>
+                                                                                                    </div>
+                                                                                                </div> \
+                                                                                    </div> ` );
+
+            $(this).closest('.input-element').find('.radio-options').append(`<div class='form-check'> \
+                                                                                                <input class='form-check-input' type='radio' value='' name='flexRadioDefault' id='flexRadioDefault1'> \
+                                                                                                <div class='input-group mb-3'>
+                                                                                                    <input type='text' class='form-control form-control-sm'>
+                                                                                                    <div class="input-group-append">
+                                                                                                        <button class='btn btn-sm rmv_option' onclick='RemoveOption()' type='button' style='color: #f30e3a'>X</i></button>
+                                                                                                    </div>
+                                                                                                </div> \
+                                                                                            </div>` );
+            //console.log(teste)
+        });
+    });
+}
+
 function RemoveInput() {
     $(document).on('click', '.rmv_btn', function () {
         // "this" is current button event occurs on
         $(this).closest('.teste-class').remove()
+    })
+}
+
+function RemoveOption() {
+    $(document).on('click', '.rmv_option', function () {
+        // "this" is current button event occurs on
+        $(this).closest('.form-check').remove()
     })
 }
 
@@ -38,27 +73,39 @@ function AddElement() {
                 $(this).closest('.teste-class').find('.input-element').replaceWith("<div class='input-element  mx-auto'><input type='text' class='form-control' id='lname' name='lname' placeholder='Entrada de Texto'></div>");
             } else if (txt == "Opções") {
                 $(this).closest('.teste-class').find('.input-element').replaceWith("<div class='input-element mx-auto'> \
+                                                                                          <div class='input-element check-options'>\
                                                                                            <div class='form-check'> \
                                                                                                <input class='form-check-input' type='checkbox' value='' name='flexCheckDefault' id='flexCheckDefault1'> \
-                                                                                               <input type='' class='form-control form-control-sm' value=''> \
+                                                                                               <div class='input-group mb-3'> \
+                                                                                                    <input type='text' class='form-control form-control-sm'> \
+                                                                                                </div>\
                                                                                            </div> \
                                                                                            <div class='form-check'> \
                                                                                               <input class='form-check-input' type='checkbox' value='' name='flexCheckDefault' id='flexCheckDefault2' checked> \
-                                                                                              <input type='' class='form-control form-control-sm' value=''> \
+                                                                                              <div class='input-group mb-3'> \
+                                                                                                    <input type='text' class='form-control form-control-sm'> \
+                                                                                                </div>\
                                                                                            </div> \
-                                                                                           <a class='btn'  onclick=''><i class='bi bi-plus' style='font-size: 2rem; color: #0099ff;'></i></a> \
+                                                                                           </div>\
+                                                                                           <button class='btn add_element' onclick='AddOption()'><i class='bi bi-plus' style='font-size: 2rem; color: #0099ff;'></i></button> \
                                                                                         </div>");
             } else if (txt == "Seleção única") {
                 $(this).closest('.teste-class').find('.input-element').replaceWith("<div class='input-element  mx-auto'> \
+                                                                                            <div class='input-element radio-options'>\
                                                                                             <div class='form-check'> \
                                                                                                 <input class='form-check-input' type='radio' value='' name='flexRadioDefault' id='flexRadioDefault1'> \
-                                                                                                <input type='' class='form-control form-control-sm' value=''> \
+                                                                                                <div class='input-group mb-3'> \
+                                                                                                    <input type='text' class='form-control form-control-sm'> \
+                                                                                                </div>\
                                                                                             </div> \
                                                                                             <div class='form-check'> \
                                                                                                 <input class='form-check-input' type='radio' value='' name='flexRadioDefault' id='flexRadioDefault1' checked> \
-                                                                                                <input type='' class='form-control form-control-sm' value=''> \
+                                                                                                <div class='input-group mb-3'> \
+                                                                                                    <input type='text' class='form-control form-control-sm'> \
+                                                                                                </div>\
                                                                                             </div> \
-                                                                                            <a class='btn'><i class='bi bi-plus' style='font-size: 2rem; color: #0099ff;'></i></a> \
+                                                                                            </div>\
+                                                                                            <button class='btn add_element' onclick='AddOption()'><i class='bi bi-plus' style='font-size: 2rem; color: #0099ff;'></i></button> \
                                                                                        </div>");
             }
         });
