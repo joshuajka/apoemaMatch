ba9b6f15a429d731109dc0867dbb6eb3229c4135
Pedro Henrique Fernandes dos Reis <pedro0798henrique@gmail.com>
2022-08-29 03:09:48 -0300

    squad2  - Cria model e view model opcaoResposta

---
+++ apoemaMatch/Data/ViewModels/OpcaoRespostaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apoemaMatch.Data.ViewModels
+{
+    public class OpcaoRespostaViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Texto para primeira opção")]
+        public string TextoPrimeiraOpcao { get; set; }
+
+        [Display(Name = "Texto para segunda opção")]
+        public string TextoSegundaOpcao { get; set; }
+
+        public bool EhRespostaEsperada { get; set; }
+    }
+}
---
+++ apoemaMatch/Models/OpcaoResposta.cs
@@ -0,0 +1,11 @@
+﻿namespace apoemaMatch.Models
+{
+    public class OpcaoResposta
+    {
+        public int Id { get; set; }
+
+        public string Texto { get; set; }
+
+        public bool EhRespostaEsperada { get; set; }
+    }
+}
